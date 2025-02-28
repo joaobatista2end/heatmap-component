a73a668fcfc2861b37acd10204503c5d07bbfc04
João Batista <joao.batista@terceirizados.farmarcas.com.br>
2025-02-28 07:51:18 -0300

    Add optional devMode prop to HeatmapProps type

--- src/Heatmap/types.ts
+++ src/Heatmap/types.ts
@@ -3,6 +3,7 @@ import type { Config, DataPoint } from "heatmap-ts"
 export type HeatmapProps = {
     data: DataPoint[],
     config?: Config,
+    devMode?: boolean,
     backgroundImage?: string | null
 }
 
